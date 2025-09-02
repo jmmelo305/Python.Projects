fcc8d04742ec4a082236f43240a9f958e95e66dd
jmmelo305 <jmmelo.edu@gmail.com>
2025-09-02 09:21:35 -0400

    Create MeloTurtleProject.py

---
+++ MeloTurtleProject.py
@@ -0,0 +1,41 @@
+import turtle
+import random 
+import time
+
+screen = turtle.Screen()
+adam = turtle.Turtle()
+adam.pu()
+adam.goto (0,-300)
+adam.pd()
+adam.pensize(20)
+adam.write ("RANDOM COLORED CIRCLE GENERATOR!")
+adam.pensize(5)
+adam.pu()
+
+
+
+def circ_les (x,y,r,color):
+    adam.goto(x,y)
+    adam.pd()
+    adam.fillcolor(color)
+    adam.begin_fill()
+    time.sleep(2)
+    adam.circle (r)
+    adam.end_fill()
+    adam.pu()
+    
+    pass
+
+def detailed (r):
+    num = random.randint(1,10)
+    list_colors = ["red","blue","yellow","green","lime","pink","#9370DB","#008B8B"]
+    for i in range(num):
+        ind = random.randint(0,(len(list_colors) - 1))
+        color = list_colors[ind]
+        random_x = random.randint(-300,300)
+        random_y = random.randint(-300,300)
+        circ_les (random_x, random_y, r, color)
+     
+        pass 
+        
+detailed (60)
